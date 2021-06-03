b1567b7aee4c676f7c89895091113a8e912239bd
marceloslo <57831311+marceloslo@users.noreply.github.com>
2021-06-03 08:40:26 -0300

    Add files via upload

--- VideoTracking.py
+++ VideoTracking.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[7]:
 
 
 from googleapiclient.discovery import build
@@ -12,7 +12,7 @@ import numpy as np
 import time
 
 
-# In[2]:
+# In[8]:
 
 
 def write_document_to_file(document, file):
@@ -21,7 +21,7 @@ def write_document_to_file(document, file):
     file.flush()
 
 
-# In[3]:
+# In[9]:
 
 
 api_key = 'AIzaSyBvMnwW7NClOkJici-WJAfVFuPusFqlRxw'
@@ -30,57 +30,58 @@ api_key2 = 'AIzaSyBYqe5QINxeNg0tf5BEzh4ngzwBiDloDlI'
 youtube2 = build('youtube', 'v3', developerKey=api_key2)
 
 
-# In[4]:
+# In[10]:
 
 
 part=["id","snippet","contentDetails","statistics","status"]
-try:
-    with open('videosData.json') as json_file:
-        videosData=[]
-        for line in json_file:
-            videosData.append(json.loads(line))
-except:
-    videosData=[]
-
-with open('videos.json') as json_file:
-    videosJ=[]
-    for line in json_file:
-        videosJ.append(json.loads(line))
-        
-with open('channels_metadata.json') as json_file:
-    channels=[]
-    for line in json_file:
-        channels.append(json.loads(line))
-
-
-# In[13]:
-
-
-US=set()
-BR=set()
-Other=set()
-for i in channels:
+while True:
     try:
-        i['id']
+        with open('videosData.json') as json_file:
+            videosData=[]
+            for line in json_file:
+                videosData.append(json.loads(line))
+    except:
+        videosData=[]
+
+    with open('videos.json') as json_file:
+        videosJ=[]
+        for line in json_file:
+            videosJ.append(json.loads(line))
+            
+    with open('channels_metadata.json') as json_file:
+        channels=[]
+        for line in json_file:
+            channels.append(json.loads(line))
+
+
+    # In[11]:
+
+
+    US=set()
+    BR=set()
+    Other=set()
+    for i in channels:
         try:
-            if i['country']=='BR':
-                BR.add(i['id'])
-            elif i['country']=='US':
-                US.add(i['id'])
-            else:
+            i['id']
+            try:
+                if i['country']=='BR':
+                    BR.add(i['id'])
+                elif i['country']=='US':
+                    US.add(i['id'])
+                else:
+                    Other.add(i['id'])
+            except:
                 Other.add(i['id'])
         except:
-            Other.add(i['id'])
-    except:
-        pass
-BR=list(BR)
-US=list(US)
-Other=list(Other)
+            pass
+    BR=list(BR)
+    US=list(US)
+    Other=list(Other)
 
 
-# In[ ]:
+    # In[12]:
 
-while True:
+
     day=date.today()
     if(date.today().weekday()==0):#adiciona possiveis novos videos sobre vacinas toda segunda
         lastupdate = datetime.utcnow()-timedelta(days=7)#ultimo update foi há uma semana
@@ -224,46 +225,48 @@ while True:
         print(newvids,' new videos found')
 
 
-    # In[ ]:
+    # In[14]:
 
 
     for l in range(len(videosJ)):
         print('Video ',l+1,' de ',len(videosJ))
+        checked=False     
         k=videosJ[l]['video']
-        Vreq=youtube2.videos().list(part=part,id=k)
-        Vquery=Vreq.execute()
-        dic={}
-        rm={}
-        if len(Vquery['items'])==0: #se a busca não encontrar nenhum video com o id pesquisado
-            for i in range(len(videosData)-1,-1,-1):
-                if videosData[i]['Video_Id']==k:
-                    if videosData[i]['Data']!='Video was removed':
-                        dic['Date']=day.strftime("%d/%m/%y")
-                        dic['Video_Id']=k
-                        dic['Data']='Video was removed'#marque como removido esse dia
-                        with open('videosData.json','a') as file:#salvando a query
-                            write_document_to_file(dic,file)
-                        rm['VideoId']=k#adicione aos videos removidos
-                        rm['RemovalDate']=day.strftime("%d/%m/%y")#adicione a data aos videos removidos
-                        for t in videosJ:
-                            if t['video']==k:
-                                rm['Channel']=t['channel']
-                                break
-                        with open('removedVideos.json','a') as file2:#salvando o video removido
-                            write_document_to_file(rm,file2)
-                    break
-                
-        else:#se lenght da query>0
-            for j in Vquery['items']:#adicione os dados normalmente 
-                dic['Date']=day.strftime("%d/%m/%y")
-                dic['Video_Id']=k
-                dic['Data']=Vquery
-                with open('videosData.json','a') as file:#salvando a query
-                    write_document_to_file(dic,file)
+        for j in range(len(videosData)-1,-1,-1):
+            if day.strftime("%d/%m/%y") == videosData[j]['Date']:
+                if k==videosData[j]['Video_Id']:
+                    checked=True
+            else:
+                break
+        if not checked:
+            Vreq=youtube2.videos().list(part=part,id=k)
+            Vquery=Vreq.execute()
+            dic={}
+            rm={}
+            if len(Vquery['items'])==0: #se a busca não encontrar nenhum video com o id pesquisado
+                for i in range(len(videosData)-1,-1,-1):
+                    if videosData[i]['Video_Id']==k:
+                        if videosData[i]['Data']!='Video was removed':
+                            dic['Date']=day.strftime("%d/%m/%y")
+                            dic['Video_Id']=k
+                            dic['Data']='Video was removed'#marque como removido esse dia
+                            with open('videosData.json','a') as file:#salvando a query
+                                write_document_to_file(dic,file)
+                            rm['VideoId']=k#adicione aos videos removidos
+                            rm['RemovalDate']=day.strftime("%d/%m/%y")#adicione a data aos videos removidos
+                            for t in videosJ:
+                                if t['video']==k:
+                                    rm['Channel']=t['channel']
+                                    break
+                            with open('removedVideos.json','a') as file2:#salvando o video removido
+                                write_document_to_file(rm,file2)
+                        break
+
+            else:#se lenght da query>0
+                for j in Vquery['items']:#adicione os dados normalmente 
+                    dic['Date']=day.strftime("%d/%m/%y")
+                    dic['Video_Id']=k
+                    dic['Data']=Vquery
+                    with open('videosData.json','a') as file:#salvando a query
+                        write_document_to_file(dic,file)
     time.sleep(86400)
-
-# In[ ]:
-
-
-
-
